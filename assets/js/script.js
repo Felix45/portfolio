08e3b10229fdd3f0acd65c572f49d3bc350dde39
Mugiira KIm <mugiirakimathi@gmail.com>
2022-05-09 18:46:20 +0300

    Add close mobile menu function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
 const hambugerMenu = document.querySelector('.menu-icon');
 const mobileMenu = document.querySelector('.menu-wrapper .menu ul');
+const closeMenu = document.querySelector('.close-icon');
+const menuItems = document.querySelectorAll('.menu-item');
 
 const toggleMenu = () => {
   mobileMenu.classList.toggle('mobile-menu');
 };
 
 hambugerMenu.addEventListener('click', toggleMenu);
-
-const closeMenu = document.querySelector('.close-icon');
 closeMenu.addEventListener('click', toggleMenu);
+menuItems.forEach((menuItem) => { menuItem.addEventListener('click', toggleMenu); });
