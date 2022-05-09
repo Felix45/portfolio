fb70bc933cafb7321530b8bdbd14193302459645
Felix Ouma <fatonoh@gmail.com>
2022-05-10 00:52:41 +0300

    Fix mobile menu display error

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
 const mobileMenu = document.querySelector('.menu-wrapper .menu ul');
 const menuItems = document.querySelectorAll('.toggle-menu');
+const statusbar = document.querySelector('.status-bar');
 
 const toggleMenu = () => {
   if (document.documentElement.clientWidth <= 767) {
     mobileMenu.classList.toggle('mobile-menu');
+    statusbar.classList.toggle('status-bar-fixed');
   }
 };
 
