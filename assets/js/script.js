2fa380563c75ec2e404c7fe2ee38b50f84421e6a
Felix Ouma <fatonoh@gmail.com>
2022-05-09 23:22:30 +0300

    Refactor mobile menu drawer feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,10 @@
-const hambugerMenu = document.querySelector('.menu-icon');
 const mobileMenu = document.querySelector('.menu-wrapper .menu ul');
-const closeMenu = document.querySelector('.close-icon');
-const menuItems = document.querySelectorAll('.menu-item');
+const menuItems = document.querySelectorAll('.toggle-menu');
 
 const toggleMenu = () => {
-  mobileMenu.classList.toggle('mobile-menu');
+  if (document.documentElement.clientWidth <= 767) {
+    mobileMenu.classList.toggle('mobile-menu');
+  }
 };
 
-hambugerMenu.addEventListener('click', toggleMenu);
-closeMenu.addEventListener('click', toggleMenu);
 menuItems.forEach((menuItem) => { menuItem.addEventListener('click', toggleMenu); });
