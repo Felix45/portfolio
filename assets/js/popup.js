4d4f5b3834e41a467e8112844a0c4e213e790dc2
Felix Ouma <fatonoh@gmail.com>
2022-05-11 20:30:32 +0300

    Fix image display and linters

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -2,12 +2,13 @@ const projects = [
   {
     title: 'Tonic',
     roles: { company: 'Canopy', position: ['Back End Dev', '2015'] },
-    image: '/assets/images/snapshootportfolio.svg',
+    image: 'assets/images/snapshootportfolio.svg',
+    alt: 'Canopy project screenshot',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
     skills: ['html', 'css', 'javascript'],
-    skillsModal: ['html', 'css', 'javascript', 'github', 'ruby', 'Bootstraps'], 
-    livelink: { link: 'felix45.github.io/portfolio', text: 'See live' },
-    seesource: { link: 'github.com/felix45/portfolio', text: 'See Source' },
+    skillsModal: ['html', 'css', 'javascript', 'github', 'ruby', 'Bootstraps'],
+    livelink: { link: 'http://felix45.github.io/portfolio', text: 'See live' },
+    seesource: { link: 'http://github.com/felix45/portfolio', text: 'See Source' },
     seeproject: 'See Project',
     htmlClass: { article: ['grid-item', 'card'], cardImage: ['card-image'], cardDescription: ['card-description'] },
   },
@@ -15,12 +16,13 @@ const projects = [
   {
     title: 'Multi-Post Stories',
     roles: { company: 'Facebook', position: ['Full Stack Dev', '2015'] },
-    image: '/assets/images/project-4.svg',
+    image: 'assets/images/project-4.svg',
+    alt: 'Multi-Post stories project screenshot',
     description: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
     skills: ['html', 'Ruby on rails', 'css', 'javascript'],
-    skillsModal: ['html', 'css', 'javascript', 'github', 'ruby', 'Bootstraps'], 
-    livelink: { link: 'felix45.github.io/portfolio', text: 'See live' },
-    seesource: { link: 'github.com/felix45/portfolio', text: 'See Source' },
+    skillsModal: ['html', 'css', 'javascript', 'github', 'ruby', 'Bootstraps'],
+    livelink: { link: 'https://felix45.github.io/portfolio', text: 'See live' },
+    seesource: { link: 'https://github.com/felix45/portfolio', text: 'See Source' },
     seeproject: 'See Project',
     htmlClass: { article: ['grid-item', 'card', 'hide-ruby'], cardImage: ['card-image', 'flex-item-2'], cardDescription: ['card-description', 'flex-item-1'] },
   },
@@ -28,12 +30,13 @@ const projects = [
   {
     title: 'Facebook 360',
     roles: { company: 'Facebook', position: ['Full Stack Dev', '2015'] },
-    image: '/assets/images/tonic.svg',
+    image: 'assets/images/tonic.svg',
+    alt: 'Facebook 360 project screenshot',
     description: 'Exploring the future of media in Facebook\'s first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
     skills: ['html', 'Ruby on rails', 'css', 'javascript'],
-    skillsModal: ['html', 'css', 'javascript', 'github', 'ruby', 'Bootstraps'], 
-    livelink: { link: 'felix45.github.io/portfolio', text: 'See live' },
-    seesource: { link: 'github.com/felix45/portfolio', text: 'See Source' },
+    skillsModal: ['html', 'css', 'javascript', 'github', 'ruby', 'Bootstraps'],
+    livelink: { link: 'https://felix45.github.io/portfolio', text: 'See live' },
+    seesource: { link: 'https://github.com/felix45/portfolio', text: 'See Source' },
     seeproject: 'See Project',
     htmlClass: { article: ['grid-item', 'card', 'flex-item-2', 'hide-ruby'], cardImage: ['card-image'], cardDescription: ['card-description'] },
   },
@@ -41,19 +44,20 @@ const projects = [
   {
     title: 'Uber Navigation',
     roles: { company: 'Uber', position: ['Lead Developer', '2018'] },
-    image: '/assets/images/multi-stories-post.svg',
+    image: 'assets/images/multi-stories-post.svg',
+    alt: 'Uber Navigation project screenshot',
     description: 'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car..',
     skills: ['html', 'Ruby on rails', 'css', 'javascript'],
-    skillsModal: ['html', 'css', 'javascript', 'github', 'ruby', 'Bootstraps'], 
-    livelink: { link: 'felix45.github.io/portfolio', text: 'See live' },
-    seesource: { link: 'github.com/felix45/portfolio', text: 'See Source' },
+    skillsModal: ['html', 'css', 'javascript', 'github', 'ruby', 'Bootstraps'],
+    livelink: { link: 'https://felix45.github.io/portfolio', text: 'See live' },
+    seesource: { link: 'http://github.com/felix45/portfolio', text: 'See Source' },
     seeproject: 'See Project',
     htmlClass: { article: ['grid-item', 'card', 'flex-item-3', 'flex-item-last', 'hide-ruby'], cardImage: ['card-image', 'flex-item-2'], cardDescription: ['card-description', 'flex-item-1'] },
   },
 ];
 
-const dummyText = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea`;
-const mobileText = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent`;
+const dummyText = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea';
+const mobileText = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent';
 
 const listItems = (items) => {
   let list = '';
@@ -64,28 +68,29 @@ const listItems = (items) => {
   return list;
 };
 
-const modalFooter = (project) => {
-  const modalButtons = document.createElement('div');
-  modalButtons.className = 'd-flex';
-  const seeSourceButton = createImageButton(project, project.livelink, "/assets/images/Icon.svg");
-  modalButtons.appendChild(seeSourceButton);
-  const seeLiveButton = createImageButton(project, project.seesource, "/assets/images/sourcecode.svg");
-  modalButtons.appendChild(seeLiveButton);
-
-  return modalButtons;
-}
-
 const createImageButton = (project, dest, srcLink) => {
   const link = document.createElement('a');
   link.className = 'btn';
-  link.textContent = `${dest.text} `
+  link.textContent = `${dest.text} `;
   link.href = dest.link;
   const img = document.createElement('img');
   img.src = srcLink;
+  img.alt = `${dest.text} Icon`;
   link.appendChild(img);
 
   return link;
-}
+};
+
+const modalFooter = (project) => {
+  const modalButtons = document.createElement('div');
+  modalButtons.className = 'd-flex';
+  const seeSourceButton = createImageButton(project, project.livelink, 'assets/images/Icon.svg');
+  modalButtons.appendChild(seeSourceButton);
+  const seeLiveButton = createImageButton(project, project.seesource, 'assets/images/sourcecode.svg');
+  modalButtons.appendChild(seeLiveButton);
+
+  return modalButtons;
+};
 
 const createRoles = (project) => {
   const cardRoleWrapper = document.createElement('div');
@@ -103,22 +108,22 @@ const createRoles = (project) => {
   cardRoleWrapper.appendChild(cardRolePosition);
 
   return cardRoleWrapper;
-}
+};
 
 const createHeader = (project, titleElement) => {
   const cardHeader = document.createElement(titleElement);
   cardHeader.textContent = project.title;
 
   return cardHeader;
-}
+};
 
 const createProjectCard = (project, position = null, isDesktop = true) => {
-  articleHolder = document.createElement('article');
+  const articleHolder = document.createElement('article');
   articleHolder.className = project.htmlClass.article.join(' ');
-  const cardHeader = createHeader(project,'h2');
+  const cardHeader = createHeader(project, 'h2');
   const cardRoleWrapper = createRoles(project);
 
-  if(!isDesktop) {
+  if (!isDesktop) {
     articleHolder.appendChild(cardHeader);
     articleHolder.appendChild(cardRoleWrapper);
   }
@@ -127,17 +132,17 @@ const createProjectCard = (project, position = null, isDesktop = true) => {
   cardImage.className = project.htmlClass.cardImage.join(' ');
   const Image = document.createElement('img');
 
-  if(isDesktop || document.documentElement.clientWidth <= 767) {
+  if (isDesktop || document.documentElement.clientWidth <= 767) {
     Image.src = project.image;
+    Image.alt = project.alt;
     cardImage.appendChild(Image);
-  }else {
-    cardImage.style.height = `568px`;
+  } else {
+    cardImage.style.height = '568px';
     cardImage.style.backgroundImage = `url(${project.image})`;
-    cardImage.style.backgroundRepeat = `no-repeat`;
-    cardImage.style.backgroundSize = `95%`;
-    cardImage.style.backgroundPositionY = `-250px`;
-    cardImage.style.backgroundPositionX = `18px`;
-
+    cardImage.style.backgroundRepeat = 'no-repeat';
+    cardImage.style.backgroundSize = '95%';
+    cardImage.style.backgroundPositionY = '-250px';
+    cardImage.style.backgroundPositionX = '18px';
 
     cardImage.classList.remove('flex-item-2');
   }
@@ -156,7 +161,7 @@ const createProjectCard = (project, position = null, isDesktop = true) => {
   const cardButton = document.createElement('a');
   cardButton.className = 'btn btn-project';
 
-  if (position){
+  if (position) {
     cardButton.setAttribute('data-position', position - 1);
   }
 
@@ -165,11 +170,11 @@ const createProjectCard = (project, position = null, isDesktop = true) => {
 
   cardTagList.innerHTML = listItems(project.skills);
 
-  if(isDesktop) {
+  if (isDesktop) {
     cardDescription.appendChild(cardHeader);
     cardDescription.appendChild(cardRoleWrapper);
     cardText.textContent = project.description;
-  }else if(document.documentElement.clientWidth <= 767){
+  } else if (document.documentElement.clientWidth <= 767) {
     cardText.textContent = mobileText;
   } else {
     cardText.textContent = dummyText;
@@ -177,20 +182,18 @@ const createProjectCard = (project, position = null, isDesktop = true) => {
   }
   cardTagWrapper.appendChild(cardTagList);
   cardDescription.appendChild(cardText);
-  if(isDesktop){
+  if (isDesktop) {
     cardButton.innerHTML = '<span>See Project</span>';
     cardTagWrapper.appendChild(cardButton);
   }
   cardDescription.appendChild(cardTagWrapper);
-  
-
   articleHolder.appendChild(cardDescription);
+
   return articleHolder;
 };
 
 const articleContainer = document.createElement('div');
 const cardContainer = document.querySelector('#port');
-//let articleHolder = null;
 
 for (let i = 0; i < projects.length; i += 1) {
   const cardInstance = createProjectCard(projects[i], i + 1, true);
@@ -199,48 +202,42 @@ for (let i = 0; i < projects.length; i += 1) {
 
 cardContainer.appendChild(articleContainer);
 
-//Popup window
-
-function showPopupWindow() {
-
- const position  = parseInt(this.getAttribute('data-position'));
- const modalContent = createProjectCard(projects[position], false, false);
-
- const overlay = document.querySelector('.overlay');
- const overlayCloseBtn = overlayCloseButton();
- const modalButtons = modalFooter(projects[position]);
-
- console.log(modalButtons);
- 
- overlay.innerHTML = '';
-
- overlay.appendChild(modalContent);
- const tagsWrapper = document.querySelector('.overlay .tags-wrapper');
- console.log(tagsWrapper);
- tagsWrapper.appendChild(modalButtons);
- overlay.childNodes[0].appendChild(overlayCloseBtn);
- 
- overlay.style.display = 'flex';
- document.querySelector('body').classList.toggle('fixed');
-}
-
 const closePopup = () => {
   const overlay = document.querySelector('.overlay');
   overlay.style.display = 'none';
   overlay.innerHTML = '';
   document.querySelector('body').classList.toggle('fixed');
-}
+};
 
 const overlayCloseButton = () => {
   const overlayCloseBtn = document.createElement('img');
   overlayCloseBtn.className = 'overlay-close';
-  overlayCloseBtn.src = '/assets/images/overlay-close.svg';
+  overlayCloseBtn.src = 'assets/images/overlay-close.svg';
+  overlayCloseBtn.alt = 'Close modal button';
 
-  overlayCloseBtn.addEventListener('click',closePopup);
+  overlayCloseBtn.addEventListener('click', closePopup);
 
   return overlayCloseBtn;
-}
+};
 
+function showPopupWindow() {
+  const position = parseInt(this.getAttribute('data-position'), 10);
+  const modalContent = createProjectCard(projects[position], false, false);
+
+  const overlay = document.querySelector('.overlay');
+  const overlayCloseBtn = overlayCloseButton();
+  const modalButtons = modalFooter(projects[position]);
+
+  overlay.innerHTML = '';
+
+  overlay.appendChild(modalContent);
+  const tagsWrapper = document.querySelector('.overlay .tags-wrapper');
+  tagsWrapper.appendChild(modalButtons);
+  overlay.childNodes[0].appendChild(overlayCloseBtn);
+
+  overlay.style.display = 'flex';
+  document.querySelector('body').classList.toggle('fixed');
+}
 
 const btnProjects = document.querySelectorAll('.btn-project');
 
