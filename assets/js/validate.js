611e1b67a4f4ef1b824cdce32959e80de8318737
Felix Ouma <fatonoh@gmail.com>
2022-05-12 22:00:46 +0300

    Add JavaScript for local storage

--- assets/js/validate.js
+++ assets/js/validate.js
@@ -1,14 +1,41 @@
+const formData = {};
 const appForms = document.forms[0];
+const userName = appForms.elements.username;
 const userEmail = appForms.elements.emailaddress;
+const message = appForms.elements.message;
+
+const saveUserInfo = () => {
+  formData['email'] = userEmail.value;
+  formData['username'] = userName.value;
+  formData['message'] = message.value;
+  localStorageSave('data', JSON.stringify(formData));
+}
+userEmail.addEventListener('blur', saveUserInfo);
+userName.addEventListener('blur', saveUserInfo);
+message.addEventListener('blur', saveUserInfo);
+
+const displayUserInfo = () => {
+  if(localStorage.getItem('data')) {
+    const userInfo = JSON.parse(localStorage.getItem('data'));
+    userName.value = userInfo.username;
+    userEmail.value = userInfo.email;
+    message.value = userInfo.message;
+  }
+}
+displayUserInfo();
 
 const validateEmail = (event) => {
   const emailError = document.querySelector('.form-error');
+  
   if (userEmail.value === userEmail.value.toLowerCase()) {
+    saveUserInfo();
     appForms.submit();
-  }
-  emailError.textContent = 'Please provide email in lowercase letters';
-  emailError.classList.add('d-flex');
-  event.preventDefault();
+  } else {
+    emailError.textContent = 'Please provide email in lowercase letters';
+    emailError.classList.add('d-flex');
+    saveUserInfo();
+    event.preventDefault();
+  } 
 };
 
 appForms.addEventListener('submit', validateEmail);
