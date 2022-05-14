cdd4c2bd409eb789312f9c0677cf568ac7a063b6
Felix Ouma <fatonoh@gmail.com>
2022-05-14 19:20:50 +0300

    Fix fixed navigation menu on scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,24 +13,31 @@ const toggleMenu = () => {
 
 menuItems.forEach((menuItem) => { menuItem.addEventListener('click', toggleMenu); });
 
+const addMenuContextualClasses = (sections) => {
+  const links = document.querySelectorAll('.menu ul li a');
+  const divOffset = 144;
+  links.forEach((el) => el.classList.remove('active'));
+
+  if (window.scrollY >= sections[0].offsetTop
+    && window.scrollY <= (sections[0].offsetTop + sections[0].offsetHeight)) {
+    links[0].classList.add('active');
+  } else if (window.scrollY >= sections[1].offsetTop
+    && window.scrollY <= (sections[1].offsetTop + sections[1].offsetHeight - divOffset)) {
+    links[1].classList.add('active');
+  } else if (window.scrollY >= sections[2].offsetTop - divOffset
+    && window.scrollY <= (sections[2].offsetTop + sections[2].offsetHeight)) {
+    links[2].classList.add('active');
+  }
+};
+
 const displayFixedMenu = () => {
   const header = document.querySelector('header');
-  const section = document.querySelectorAll('.page-section');
+  const sections = document.querySelectorAll('.page-section');
 
   const isFixed = this.window.scrollY > 200 ? header.classList.add('header-fixed') : header.classList.remove('header-fixed');
 
-  const links = document.querySelectorAll('.menu ul li a');
-  links.forEach((el) => el.classList.remove('active'));
-
-  if (window.scrollY >= section[0].offsetTop
-    && window.scrollY <= (section[0].offsetTop + section[0].offsetHeight)) {
-    links[0].classList.add('active');
-  } else if (window.scrollY >= section[1].offsetTop
-    && window.scrollY <= (section[1].offsetTop + section[1].offsetHeight)) {
-    links[1].classList.add('active');
-  } else if (window.scrollY >= section[2].offsetTop
-    && window.scrollY <= (section[2].offsetTop + section[2].offsetHeight)) {
-    links[2].classList.add('active');
+  if (document.documentElement.clientWidth >= 767) {
+    addMenuContextualClasses(sections);
   }
 
   return isFixed;
