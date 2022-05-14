9f7c7d1288ff4be1872231c154368309e0e54114
Felix Ouma <fatonoh@gmail.com>
2022-05-14 12:43:33 +0300

    Update contributors and tech used list on readme.md file

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -193,7 +193,7 @@ const createProjectCard = (project, position = null, isDesktop = true) => {
 };
 
 const articleContainer = document.createElement('div');
-const cardContainer = document.querySelector('#port');
+const cardContainer = document.querySelector('#portfolio');
 
 for (let i = 0; i < projects.length; i += 1) {
   const cardInstance = createProjectCard(projects[i], i + 1, true);
