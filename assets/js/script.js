3ba7f95b84ada1feaa2c0461757b353519190fe6
Felix Ouma <fatonoh@gmail.com>
2022-05-14 15:07:40 +0300

    Add contextual classes to menu items on scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,7 +15,23 @@ menuItems.forEach((menuItem) => { menuItem.addEventListener('click', toggleMenu)
 
 const displayFixedMenu = () => {
   const header = document.querySelector('header');
+  const section = document.querySelectorAll('.page-section');
+
   const isFixed = this.window.scrollY > 200 ? header.classList.add('header-fixed') : header.classList.remove('header-fixed');
+
+  const links = document.querySelectorAll('.menu ul li a');
+  links.forEach((el) => el.classList.remove('active'));
+
+  if (window.scrollY >= section[0].offsetTop
+    && window.scrollY <= (section[0].offsetTop + section[0].offsetHeight)) {
+    links[0].classList.add('active');
+  } else if (window.scrollY >= section[1].offsetTop
+    && window.scrollY <= (section[1].offsetTop + section[1].offsetHeight)) {
+    links[1].classList.add('active');
+  } else if (window.scrollY >= section[2].offsetTop
+    && window.scrollY <= (section[2].offsetTop + section[2].offsetHeight)) {
+    links[2].classList.add('active');
+  }
 
   return isFixed;
 };
